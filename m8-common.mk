0887bcb53e0299df9df46121ca61e678789ca68b
Raja M <raja1612.m@gmail.com>
2016-04-05 11:13:03 +0530

    Enforcing vendors files if it misses build will fail

--- m8-common.mk
+++ m8-common.mk
@@ -14,7 +14,7 @@
 # limitations under the License.
 #
 
-$(call inherit-product-if-exists, vendor/htc/m8-common/m8-common-vendor.mk)
+$(call inherit-product, vendor/htc/m8-common/m8-common-vendor.mk)
 
 # Inherit from msm8974-common
 $(call inherit-product, device/htc/msm8974-common/msm8974-common.mk)
